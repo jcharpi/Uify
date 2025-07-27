6156119d5dd87b93d6e4669146bdcf9f0dbaa49d
Josh Charpentier <charpentier.josh@gmail.com>
2025-07-27 11:36:15 -0500

    formatting document

--- src/app/prompt.tsx
+++ src/app/prompt.tsx
@@ -24,13 +24,30 @@ export default function Prompt() {
           padding: "var(--size-responsive-20)",
           backgroundColor: "var(--palette-default-surface-1)",
           height: "60vh",
-          width: "60vh",
+          width: "75vh",
           maxWidth: "90vw",
           margin: "10vh auto 0 auto",
           borderRadius: "var(--size-rounded-medium)",
         }}
       >
-
+        <h2
+          className="v-typography-headline-1"
+          style={{
+            color: "var(--palette-default-text)",
+            textAlign: "center",
+          }}
+        >
+          Component Generator
+        </h2>
+        <h3
+          className="v-typography-subtitle-1 v-typography-color-subtle"
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Describe your UI component and get React Code that makes use of
+          Visa&apos;s Design System
+        </h3>
       </div>
     </div>
   )
